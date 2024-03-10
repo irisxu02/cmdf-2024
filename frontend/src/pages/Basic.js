d1ebc0a0cd2b1e37f1e28cacd5968952108f94c1
LenaWang03 <xianna03@gmail.com>
2024-03-09 19:26:04 -0800

    merge conflicts

--- frontend/src/pages/Basic.js
+++ frontend/src/pages/Basic.js
@@ -33,6 +33,91 @@ const Basic = () => {
                 console.error("Error:", error);
             });
     };
+  const [promptContent, setPromptContent] = useState(
+    <div id="prompt" className="prompt">
+      <div className="subtext">Explain</div>
+      <input
+        type="text"
+        id="explain"
+        name="explain"
+        className="fullWidthInput"
+        placeholder="type something you want to know here..."
+      />
+      <div className="subtext">To Me Like I'm</div>
+      <select id="age" name="age" className="dropdown">
+        <option value="0-10">a Toddler</option>
+        <option value="11-20">a Child</option>
+        <option value="21-30">a Teenager</option>
+        <option value="31-40">an Adult</option>
+      </select>
+    </div>
+  );
+  
+  const [basicButtonClass, setBasicButtonClass] = useState("subheading gradientFont");
+  const [advancedButtonClass, setAdvancedButtonClass] = useState("subheading gradientFont grey");
+
+  const handleBasicClick = () => {
+    setPromptContent(
+      <div id="prompt" className="prompt">
+        <div className="subtext">Explain</div>
+        <input
+          type="text"
+          id="explain"
+          name="explain"
+          className="fullWidthInput"
+          placeholder="type something you want to know here..."
+        />
+        <div className="subtext">To Me Like I'm</div>
+        <select id="age" name="age" className="dropdown">
+          <option value="0-10">a Toddler</option>
+          <option value="11-20">a Child</option>
+          <option value="21-30">a Teenager</option>
+          <option value="31-40">an Adult</option>
+        </select>
+      </div>
+    );
+    setBasicButtonClass("subheading gradientFont");
+    setAdvancedButtonClass("subheading gradientFont grey");
+  };
+
+  const handleAdvancedClick = () => {
+    setPromptContent(
+      <div className="prompt">
+        <div className="subtext">Explain</div>
+        <input
+          type="text"
+          id="explain"
+          name="explain"
+          class="fullWidthInput"
+          placeholder="type something you want to know here..."
+        />
+        <div className="subtext">To Me Like I'm</div>
+        <select id="age" name="age" class="dropdown">
+          <option value="0-10">a Toddler</option>
+          <option value="11-20">a Child</option>
+          <option value="21-30">a Teenager</option>
+          <option value="31-40">an Adult</option>
+        </select>
+        <div className="subtext">who is a </div>
+        <input
+          type="text"
+          id="explain"
+          name="explain"
+          class="inputField"
+          placeholder="role type"
+        />
+        <div className="subtext">in a </div>
+        <select id="length" name="age" class="dropdown">
+          <option value="sentence">sentence.</option>
+          <option value="paragraph">paragraph.</option>
+          <option value="page">page.</option>
+        </select>
+      </div>
+    );
+    setBasicButtonClass("subheading gradientFont grey");
+    setAdvancedButtonClass("subheading gradientFont");
+  };
+
   return (
     <>
       <main className="basicContent">
@@ -43,28 +128,25 @@ const Basic = () => {
           <div className="subheading">EITMLI.</div>
         </div>
         <div className="basicPrompt">
-          <div className="subheading gradientFont">BASIC</div>
-          <div className="prompt">
-            <div className="subtext">Explain</div>
-            <input
-              type="text"
-              id="explain"
-              name="explain"
-              class="fullWidthInput"
-              placeholder="type something you want to know here..."
-              onChange={handleInputChange}
-              value={inputValue}
-            />
-            <VoiceInput setInput={setInputValue}/>
-            <div className="subtext">To Me Like I'm</div>
-            <select id="age" name="age" class="dropdown" onChange={handleAgeChange}>
-              <option value="0-10">a Toddler</option>
-              <option value="11-20">a Child</option>
-              <option value="21-30">a Teenager</option>
-              <option value="31-40">an Adult</option>
-            </select>
-            <button onClick={handleSubmit}>submit</button>
-            <p>{response}</p>
+          <div className="views">
+            <div
+              id="basic"
+              className={basicButtonClass}
+              onClick={handleBasicClick}
+            >
+              BASIC
+            </div>
+            <div
+              id="advanced"
+              className={advancedButtonClass}
+              onClick={handleAdvancedClick}
+            >
+              ADVANCED
+            </div>
+          </div>
+          {promptContent}
+          <div className="center">
+              <button className="fancy center">EXPLAIN</button>
           </div>
         </div>
       </main>
