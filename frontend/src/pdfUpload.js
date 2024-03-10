64f0dddf4f5945de28dfff57513a40f43d0380a2
isa-leroux448 <isa.leroux448@gmail.com>
2024-03-10 09:01:57 -0700

    pdf upload done

--- frontend/src/pdfUpload.js
+++ frontend/src/pdfUpload.js
@@ -2,37 +2,11 @@ import React, { useState } from 'react';
 import { FaFileUpload } from "react-icons/fa";
 import { FaRegCheckCircle } from "react-icons/fa";
 
-const PDFUpload = ({setInput}) => {
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [uploadStatus, setUploadStatus] = useState(null);
-
+const PDFUpload = ({setInput, setPDF, pdf}) => {
   const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
-    setInput(event.target.files[0].name)
-    // Reset upload status when a new file is selected
-    setUploadStatus(null);
-  };
-
-  const handleSubmit = async () => {
-    const formData = new FormData();
-    formData.append('pdfFile', selectedFile);
-
-    try {
-      const response = await fetch("http://127.0.0.1:4999/upload", {
-        method: 'POST',
-        body: formData,
-      });
-
-      if (response.ok) {
-        setUploadStatus('success');
-        console.log('File uploaded successfully:', response);
-      } else {
-        setUploadStatus('error');
-        console.error('Error uploading file:', response.statusText);
-      }
-    } catch (error) {
-      setUploadStatus('error');
-      console.error('Error uploading file:', error);
+    if (event.target.files[0]) {
+      setPDF(event.target.files[0]);
+      setInput(event.target.files[0].name)
     }
   };
 
@@ -41,7 +15,7 @@ const PDFUpload = ({setInput}) => {
       <label htmlFor="fileInput">
         <input htmlFor="fileInput" id="fileInput" type="file" onChange={handleFileChange} accept=".pdf" style={{ display: "none" }} />
         <span className="icon">
-          {selectedFile ? <FaRegCheckCircle style={{ color: "0B91E9" }} /> :  <FaFileUpload style={{ color: "white" }} />}
+          {pdf ? <FaRegCheckCircle style={{ color: "0B91E9" }} /> :  <FaFileUpload style={{ color: "white" }} />}
           </span>
     </label>
     </div>
